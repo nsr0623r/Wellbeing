a16f087485af78f76f9eaa21c2a1a5ad09a59dbb
nsr0623r <nsr0623r@yahoo.co.jp>
2023-03-04 10:51:20 +0000

    [add]public/homes

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,12 @@ Rails.application.routes.draw do
   devise_for :admins, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
+  
+  # ユーザー側devise
+  scope module: :public do
+    root to: 'homes#top'
+    get '/about' => 'homes#about', as: 'about'
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
