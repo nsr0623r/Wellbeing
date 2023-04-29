daf5cb0c0c44ec1746b1cf0d2e5ab724f55ef17b
nsr0623r <nsr0623r@yahoo.co.jp>
2023-04-29 22:56:19 +0000

    [add]user

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -3,13 +3,13 @@
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
   
-  # def after_sign_in_path_for(resource)
-  #   user_my_page_path(resource)
-  # end
+  def after_sign_in_path_for(resource)
+    my_page_users_path(resource)
+  end
   
-  # def after_sign_out_path_for(resource)
-  #   root_path
-  # end
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 
   # GET /resource/sign_in
   # def new
