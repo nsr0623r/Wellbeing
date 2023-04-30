f3d9b697377b07494bc720133f9ab902ff7a7f4c
nsr0623r <nsr0623r@yahoo.co.jp>
2023-04-30 05:02:21 +0000

    [add]admin/post

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,10 +1,28 @@
 class Admin::PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to admin_post_path
+    else
+      render :edit
+    end
+  end
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:release)
   end
 end
